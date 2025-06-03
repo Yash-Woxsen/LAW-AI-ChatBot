9d7a6ed5f94dfcd10516bcd2e68e404842191a2e
Yash Office PC <167862397+Yash-Woxsen@users.noreply.github.com>
2025-06-03 15:45:44 +0530

    Web Build Update

--- Assets/_Scripts/UI/ToggleSuggestionPanel.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class ToggleSuggestionPanel : MonoBehaviour
-{
-    public void TogglePanel(GameObject panelToToggle)
-    {
-        panelToToggle.SetActive(!panelToToggle.activeInHierarchy);
-    }
-}
